e0bc94880eeea6b76168a2534717e3a4d9093886
Angel Borja <borjaangel244@gmail.com>
2023-04-25 14:55:18 -0400

    update

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -52,8 +52,12 @@ public class PhoneBook {
     }
 
     public String reverseLookup(String phoneNumber) {
-        return null;
-
+        for (Map.Entry<String, List<String>> entry : phonebook.entrySet()) {
+            if (entry.getValue().contains(phoneNumber)) {
+                return entry.getKey();
+            }
+        }
+        return "Invalid Phone Number";
     }
 
     public List<String> getAllContactNames() {
@@ -65,6 +69,6 @@ public class PhoneBook {
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return phonebook;
     }
 }
